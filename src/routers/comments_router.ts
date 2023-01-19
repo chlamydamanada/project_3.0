7a41f4897532582101932c0bf28ebf198e8cceff
chlamydamanada <evil.cabaret@gmail.com>
2023-01-19 22:40:52 +0100

    comment controller

--- src/routers/comments_router.ts
+++ src/routers/comments_router.ts
@@ -1,4 +1,4 @@
-import {Request, Response, Router} from "express";
+import {Response, Router} from "express";
 import {commentsQweryRepository} from "../repositories/comments_qwery_repository";
 import {commentsService} from "../domain/comments_service";
 import {bearerAuthMiddleware} from "../middlewares/bearerAuthrization.middleware";
@@ -11,28 +11,25 @@ import {commentStatusValidation} from "../middlewares/commentStatus.middleware";
 
 export const commentsRouter = Router();
 
-commentsRouter.get("/:commentId",
-    async (req: RequestWithURL<{ commentId: string }>,
-                   res: Response<commentViewType | string>) => {
-    try {
-        const comment = await commentsQweryRepository.findCommentById(
-            req.params.commentId
-        );
-        if (!comment) {
-            res.sendStatus(404);
-        } else {
-            res.status(200).send(comment);
+class CommentsController {
+    async getCommentById(req: RequestWithURL<{ commentId: string }>,
+                         res: Response<commentViewType | string>) {
+        try {
+            const comment = await commentsQweryRepository.findCommentById(
+                req.params.commentId
+            );
+            if (!comment) {
+                res.sendStatus(404);
+            } else {
+                res.status(200).send(comment);
+            }
+        } catch (e) {
+            res.status(500).send("commentsRouter.get/:commentId" + e)
         }
-    } catch (e) {
-        res.status(500).send("commentsRouter.get/:commentId" + e)
     }
-});
-commentsRouter.delete(
-    "/:commentId",
-    bearerAuthMiddleware,
-    userIsOwnerOfCommentMiddleware,
-    async (req: RequestWithURL<{ commentId: string }>,
-           res: Response<string>) => {
+
+    async deleteCommentById(req: RequestWithURL<{ commentId: string }>,
+                            res: Response<string>) {
         try {
             await commentsService.deleteComment(req.params.commentId);
             res.sendStatus(204);
@@ -40,15 +37,9 @@ commentsRouter.delete(
             res.status(500).send("commentsRouter.delete/:commentId" + e)
         }
     }
-);
-commentsRouter.put(
-    "/:commentId",
-    bearerAuthMiddleware,
-    userIsOwnerOfCommentMiddleware,
-    contentOfCommentsMiddleware,
-    inputValMiddleware,
-    async (req: RequestWithURL<{ commentId: string }>,
-           res: Response<string>) => {
+
+    async updateCommentById(req: RequestWithURL<{ commentId: string }>,
+                            res: Response<string>) {
         try {
             const newComment = await commentsService.updateComment(
                 req.params.commentId,
@@ -61,27 +52,45 @@ commentsRouter.put(
             res.status(500).send("commentsRouter.put/:commentId" + e)
         }
     }
-);
-commentsRouter.put(
-    "/:commentId/like-status",
-    bearerAuthMiddleware,
-    commentStatusValidation,
-    inputValMiddleware,
-    async (req: RequestWithUrlAndBody<{ commentId: string }, {likeStatus: string}>,
-           res: Response<string>) => {
+
+    async updateLikeStatusByCommentId(req: RequestWithUrlAndBody<{ commentId: string }, { likeStatus: string }>,
+                                      res: Response<string>) {
         try {
             const comment = await commentsQweryRepository.findCommentById(req.params.commentId);
-           if (!comment){
-               res.sendStatus(404);
-           }
-           await commentsService.generateStatusOfComment(
-               req.params.commentId,
-               req.user!.id,
-               req.body.likeStatus)
-                res.sendStatus(204);
+            if (!comment) {
+                res.sendStatus(404);
+            }
+            await commentsService.generateStatusOfComment(
+                req.params.commentId,
+                req.user!.id,
+                req.body.likeStatus)
+            res.sendStatus(204);
 
         } catch (e) {
             res.status(500).send("commentsRouter.put/:commentId" + e)
         }
     }
-);
+}
+
+const commentsController = new CommentsController();
+
+commentsRouter.get("/:commentId",
+    commentsController.getCommentById.bind(commentsController));
+commentsRouter.delete(
+    "/:commentId",
+    bearerAuthMiddleware,
+    userIsOwnerOfCommentMiddleware,
+    commentsController.deleteCommentById.bind(commentsController));
+commentsRouter.put(
+    "/:commentId",
+    bearerAuthMiddleware,
+    userIsOwnerOfCommentMiddleware,
+    contentOfCommentsMiddleware,
+    inputValMiddleware,
+    commentsController.updateCommentById.bind(commentsController));
+commentsRouter.put(
+    "/:commentId/like-status",
+    bearerAuthMiddleware,
+    commentStatusValidation,
+    inputValMiddleware,
+    commentsController.updateLikeStatusByCommentId.bind(commentsController));
